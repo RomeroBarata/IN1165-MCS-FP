6362903e82059659bb86fd384db7e8954fefa741
RomeroBarata <romero.barata@gmail.com>
2016-11-05 13:42:03 -0300

    Function to automatically determine the number of clusters and clusterise the examples, using a som and a kmeans

---
+++ R/som-functions.R
@@ -0,0 +1,30 @@
+clusters <- function(x){
+  if (!is.matrix(x)) x <- as.matrix(x)
+  x <- scale(x)  # Center and scale the attributes
+  
+  num_examples <- nrow(x)
+  if (num_examples <= 100) 
+    xdim <- ydim <- 4
+  else if (num_examples <= 1000)
+    xdim <- ydim <- 8
+  else if (num_examples <= 5000) 
+    xdim <- ydim <- 16
+  else 
+    xdim <- ydim <- 20
+  
+  som_grid <- somgrid(xdim = xdim, ydim = ydim, topo = "hexagonal")
+  som_model <- som(x, 
+                   grid = som_grid, 
+                   rlen = 100, 
+                   alpha = c(0.05, 0.01), 
+                   n.hood = "circular")
+  
+  wss <- vapply(1:15, 
+                function(i) sum(kmeans(som_model[["codes"]], 
+                                       centers = i)[["withinss"]]), 
+                numeric(1))
+  num_clusters <- which.min(abs(wss - (0.25 * wss[1] + 0.75 * wss[15])))
+  # cutree(hclust(dist(som_model$codes)), num_clusters)
+  kmeans_model <- kmeans(x, centers = num_clusters, iter.max = 100, nstart = 10)
+  unname(kmeans_model[["cluster"]])
+}
