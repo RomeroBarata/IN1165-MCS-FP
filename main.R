a7d39dca61db5b52d0e553e96c3b2689fa481f09
RomeroBarata <romero.barata@gmail.com>
2016-11-15 14:54:09 -0300

    Current main

--- main.R
+++ main.R
@@ -9,19 +9,15 @@ R_PATH <- "R"
 
 ## Source defined functions ----------------------------------------------------
 source(file.path(R_PATH, "boosting-functions.R"))
+source(file.path(R_PATH, "cv-functions.R"))
 source(file.path(R_PATH, "data-functions.R"))
 source(file.path(R_PATH, "neural-net-functions.R"))
 source(file.path(R_PATH, "sampling-functions.R"))
 source(file.path(R_PATH, "som-functions.R"))
 source(file.path(R_PATH, "util-functions.R"))
 
-## 
-iono <- read.csv(file.path(DATA_PATH, "hepatitis.csv"), header = TRUE)
-data_maj <- subset(iono, Class == -1)
-data_min <- subset(iono, Class == 1)
-maj_clusters <- clusters(data_maj[, -ncol(data_maj)])
-min_clusters <- clusters(data_min[, -ncol(data_min)])
+## Import data sets ------------------------------------------------------------
+data_list <- readDataSets(DATA_PATH)
 
-boundaries <- identifyBoundary(data_maj, data_min, maj_clusters, min_clusters)
-synth <- synthesiseBoundary(data_maj[, -ncol(data_maj)], data_min[, -ncol(data_min)], 
-                            boundaries$Boundary_Majority, boundaries$Boundary_Minority)
+## Tests
+breast <- data_list[[1]]
